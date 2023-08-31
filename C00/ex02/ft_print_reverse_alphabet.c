51c1c18693be9da09564b1b29fc09ad7e2f02417
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2023-08-31 06:38:14 -0300

    ex02

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 06:31:52 by yufonten          #+#    #+#             */
+/*   Updated: 2023/08/31 06:34:38 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		ft_putchar(c)
+		c--;
+	}
+}
