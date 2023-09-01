47c043d3d97cf57419c1248a514d39570cbb4539
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2023-09-01 10:23:27 -0300

    C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 09:58:41 by yufonten          #+#    #+#             */
+/*   Updated: 2023/09/01 10:03:53 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	n;
+	char	n2;
+	char	n3;
+
+	n = '0';
+	while (n < '8')
+	{
+		n2 = n + 1;
+		while (n2 < '9')
+		{
+			n3 = n2 + 1;
+			while (n3 <= '9')
+			{
+				ft_putchar(n);
+				ft_putchar(n2);
+				ft_putchar(n3);
+				if (n != '7')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				n3++;
+			}
+			n2++;
+		}
+		n++;
+	}
+}
+
+int main(void)
+{
+	ft_print_comb();
+}
---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 10:11:11 by yufonten          #+#    #+#             */
+/*   Updated: 2023/09/01 10:18:26 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
