0dee446790edcbb4c0ffe711a52f4d9c540a9e0c
YuriFont <yurifontenelesilva180303@gmail.com>
2023-09-10 14:23:56 -0300

    c02

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 00:40:14 by yufonten          #+#    #+#             */
-/*   Updated: 2023/09/06 02:23:14 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/09/10 14:23:42 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,6 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
 }
 
 int	ft_char_is_printable(char c)
