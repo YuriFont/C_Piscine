40aa4dd96a4a3840b3a56489b23c0ce5bbc43274
YuriFont <yurifontenelesilva180303@gmail.com>
2023-09-11 19:37:14 -0300

    c08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 18:24:00 by yufonten          #+#    #+#             */
+/*   Updated: 2023/09/11 19:36:58 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+
+#endif
