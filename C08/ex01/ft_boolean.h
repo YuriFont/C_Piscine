ad4e23930d39126a43096e674d750c50c49f3909
YuriFont <yurifontenelesilva180303@gmail.com>
2023-09-12 18:31:58 -0300

    c08

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,15 +6,26 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 18:24:00 by yufonten          #+#    #+#             */
-/*   Updated: 2023/09/11 19:36:58 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/09/12 18:31:01 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
+# include <unistd.h>
 
+typedef enum s_boolean
+{
+	false = 0,
+	true,
+}	t_bool;
+
+# define FALSE false
+# define TRUE true
+# define EVEN(nbr) (nbr % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments."
 # define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
 
 #endif
